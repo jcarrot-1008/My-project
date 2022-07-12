2da5de8cd8ede54915b45f00869f1c794546779f
kjs <kjs@logiall.com>
2022-07-12 18:40:06 +0900

    cors 정책 수정

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,11 +5,7 @@ import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  app.enableCors();
   const serverConfig = config.get('server');
   const port = serverConfig.port;
   await app.listen(port);
