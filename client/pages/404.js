481679cdd85938c45d5bf60f291a174ce88b5c60
kjs <kjs@logiall.com>
2022-08-05 10:20:46 +0900

    first commit

---
+++ client/pages/404.js
@@ -0,0 +1,7 @@
+export default function PageNotFound(){
+    return(
+        <>
+            <h1 style={{display: 'flex', justifyContent: "center", alignItems: 'center', height: '90vh'}}>페이지를 찾을수 없습니다</h1>
+        </>
+    );
+}
