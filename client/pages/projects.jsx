86b48192c15b579cef28237414c53115a549c472
kjs <kjs@logiall.com>
2022-08-05 17:58:02 +0900

    업데이트 테스트

--- client/pages/projects.jsx
+++ client/pages/projects.jsx
@@ -24,10 +24,6 @@ function Projects(props) {
                     총 프로젝트 :
                     <span className="pl-4 text-blue-500">{projects.results.length}</span>
                 </h1>
-        <button onClick={loginModel.setInputField("email",'aa')}>
-            test
-        </button>
-                
                     {
                     detailPageId !== "list" ?
                     <div className="grid grid-cols-1 gap-8 p-12 m-4 md:grid-cols-1">
