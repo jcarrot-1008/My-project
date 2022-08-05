297b53235c156236ae1c11053f75380bcb4276c2
kjs <kjs@logiall.com>
2022-08-05 10:08:38 +0900

    Merge branch 'master'
    # Conflicts:
    #	client/components/projects/project-item.js
    #	client/package-lock.json
    #	client/package.json
    #	client/pages/projects.js

--- client/pages/404.js
+++
@@ -1,7 +0,0 @@
-export default function PageNotFound(){
-    return(
-        <>
-            <h1 style={{display: 'flex', justifyContent: "center", alignItems: 'center', height: '90vh'}}>페이지를 찾을수 없습니다</h1>
-        </>
-    );
-}
